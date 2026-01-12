6ac13323178f72b02dbf81ea4d7e9e0d8421c43d
winverse <public.winverse@gmail.com>
2026-01-12 11:11:11 +0900

    feat: add cors

--- 11-completed/src/server.js
+++ 11-completed/src/server.js
@@ -3,7 +3,7 @@ import { prisma } from './db/prisma.js';
 import { config } from '#config';
 import { router as apiRouter } from './routes/index.js';
 import cookieParser from 'cookie-parser';
-import { errorHandler } from '#middlewares';
+import { errorHandler, cors } from '#middlewares';
 import { setupGracefulShutdown } from '#utils';
 
 const app = express();
@@ -13,6 +13,9 @@ app.use(express.json());
 
 // 쿠키 파싱 (중요!)
 app.use(cookieParser());
+
+// cors
+app.use(cors);
 
 // API 라우터 등록
 app.use('/api', apiRouter);
